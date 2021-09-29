1083136e3b9a7b3dd7cc1aa1ad916f63cf02aada
Gabriel Oliveira <gabrielosouza21@gmail.com>
2021-09-29 19:04:30 -0300

    cmp2.c added

---
+++ basic_math/cmp2.c
@@ -0,0 +1,24 @@
+/*this program will verify if the sum of the square of the numbers are
+equal to the square of their sum*/
+//this is another way to use if statement
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <math.h>
+
+int main(void) {
+
+    int a, b, cmp1, cmp2;
+
+    printf("Type a number :");
+    scanf("%d", & a);
+    printf("Type another number :");
+    scanf("%d", & b);
+
+    cmp1 = pow(a, 2) + pow(b, 2);
+    cmp2 = pow(a + b, 2);
+
+    (cmp1 == cmp2)?printf("They're equal !\n"):printf("They're not equal\n");
+}
